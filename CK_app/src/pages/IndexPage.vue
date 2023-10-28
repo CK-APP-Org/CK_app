9566052a136f0f38c56e31a0a3f5191704bb65a6
Kimi Yang <plantandanimalandinsectjh@gmail.com>
2023-10-28 11:54:41 +0800

    added some feathers related to class schedule

--- CK_app/src/pages/IndexPage.vue
+++ CK_app/src/pages/IndexPage.vue
@@ -1,9 +1,14 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
+    <div style="max-width: 200px;" class="q-gutter-md">
+      <q-input filled v-model="userTempClass" label="Class(ex:227)" />
+      <q-btn color="secondary" label="Confirm" @click="confirmClass"/>
+    </div>
+    <h4>{{userClass}} schedule</h4>
     <img
       alt="Quasar logo"
       src="~assets/227schedule.png"
-      style="width: 400px; height: 500px"
+      style="width: 450px; height: 600px"
     />
   </q-page>
 </template>
@@ -11,7 +16,18 @@
 <script>
 import { defineComponent } from "vue";
 
-export default defineComponent({
-  name: "IndexPage",
-});
+export default{
+  data(){
+    return{
+      userClass:'000',
+      userTempClass: '227'
+    }
+  },
+  methods: {
+    confirmClass(){
+      alert("Changing your class number to "+this.userTempClass)
+      this.userClass = this.userTempClass
+    }
+  }
+}
 </script>

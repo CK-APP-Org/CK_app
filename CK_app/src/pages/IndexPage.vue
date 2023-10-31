f25b908d49c0fa2bba4f1a59b4f938eb949fc014
Kimi Yang <plantandanimalandinsectjh@gmail.com>
2023-10-31 22:24:31 +0800

    f

--- CK_app/src/pages/IndexPage.vue
+++ CK_app/src/pages/IndexPage.vue
@@ -6,9 +6,14 @@
         <q-btn color="secondary" label="確認班級" @click="confirmClass" />
       </div>
     </div>
-    <h4 class="beautiful-text">{{ userClass }} 課表</h4>
-
-    <h4 class="beautiful-text">{{ data }}</h4>
+    <h4 class="beautiful-text">{{ userClass }} 課表, double click class to customize</h4>
+    <div class="table-container">
+      <table class="responsive-table">
+        <tr v-for="row in scheduleData[userClass]" :key="row">
+          <td class="class-text" v-for="col in row" :key="col" @dblclick="customLabel">{{ col }}</td>
+        </tr>
+      </table>
+    </div>
     <img
       alt="class schedule"
       :src="getClassScheduleImageURL"
@@ -16,6 +21,7 @@
     />
   </q-page>
 </template>
+
 
 <style scoped>
 .input-container {
@@ -35,14 +41,35 @@
 
 .beautiful-text {
   font-size: 24px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   color: #007bff;
   font-weight: bold;
   text-transform: uppercase;
   text-decoration: underline;
-  font-family: "Arial", sans-serif; /* Use a specific font */
-  letter-spacing: 2px; /* Increase letter spacing */
-  margin-top: 10px; /* Add spacing above the text */
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); /* Add a text shadow */
+  letter-spacing: 2px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+
+.class-text{
+  font-size: 20px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: #fc7e00;
+  font-weight: bold;
+  letter-spacing: auto;
+}
+.responsive-table {
+  border-collapse: collapse;
+  width: 100%; /* Make the table width 100% of its container */
+  height: 500px;
+}
+
+.responsive-table td {
+  border: 1px solid #000;
+  padding: 5px;
+  text-align: center;
+}
+.table-container {
+  overflow-x: auto; /* Add horizontal scrollbar if the table overflows the screen */
 }
 </style>
 <script>
@@ -52,8 +79,9 @@ import jsonData from "src/assets/data.json";
 export default {
   data() {
     return {
-      userClass: "000",
-      userTempClass: "227",
+      userClass: 227,
+      userTempClass: 227,
+      scheduleData: jsonData,
     };
   },
   methods: {
@@ -61,6 +89,9 @@ export default {
       alert("成功更改班級為" + this.userTempClass);
       this.userClass = this.userTempClass;
     },
+    customLabel() {
+      alert("Still in development")
+    }
   },
   computed: {
     getClassScheduleImageURL() {

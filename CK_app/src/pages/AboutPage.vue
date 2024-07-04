8620e74c30326ec1ee8ffeffcc4390c69bd890b4
Kimi Yang <plantandanimalandinsectjh@gmail.com>
2024-07-04 22:01:18 +0800

    improved schedule interface, carousel for about page WIP

--- CK_app/src/pages/AboutPage.vue
+++ CK_app/src/pages/AboutPage.vue
@@ -1,10 +1,62 @@
 <template>
-  <q-page
-    >About</q-page
-  >
+  <q-page>
+    <q-carousel
+        v-model="slide"
+        transition-prev="jump-right"
+        transition-next="jump-left"
+        swipeable
+        animated
+        control-color="white"
+        prev-icon="arrow_left"
+        next-icon="arrow_right"
+        navigation-icon="radio_button_unchecked"
+        navigation
+        padding
+        arrows
+        height="300px"
+        class="bg-purple text-white shadow-1 rounded-borders"
+      >
+      <q-carousel-slide name="style" class="column no-wrap flex-center">
+        <q-icon name="help" size="56px" />
+        <div class="q-mt-md text-center">
+          CK APP，您專屬的校園助理，你可以在這裡了解更多說明!!!
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="tv" class="column no-wrap flex-center">
+        <q-icon name="live_tv" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="layers" class="column no-wrap flex-center">
+        <q-icon name="layers" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="map" class="column no-wrap flex-center">
+        <q-icon name="terrain" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </q-page>
 </template>
 
-<style></style>
+<style>
+</style>
+
+
 <script>
-export default {};
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    return{
+      slide: ref('style'),
+      lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
+    }
+  }
+};
 </script>

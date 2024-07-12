fa3ebcec7bbc17e7058a102d5ef9e91041cad38b
Kimi Yang <plantandanimalandinsectjh@gmail.com>
2024-07-12 10:37:57 +0800

    bug fix on about, add official gmail send, minor change on home

--- CK_app/src/pages/AboutPage.vue
+++ CK_app/src/pages/AboutPage.vue
@@ -37,7 +37,7 @@
       <q-carousel-slide name="Youbike" class="column no-wrap flex-center">
         <q-icon name="directions_bike" size="56px" />
         <div class="q-mt-md text-center text-h5">
-          Youbike : 便捷查詢，自行車輛隨時掌握
+          Youbike : 便捷查詢，車輛隨時掌握
         </div>
         <div class="q-mt-md text-center">
           相較於官方APP，在這裡你不僅僅能獲得各站點的即時資訊，還能客製化優先顯示的站點資料並更改暱稱
@@ -58,7 +58,10 @@
 
     <q-banner class="bg-secondary text-white">
       <div class="text-h5">關於這個APP</div>
-      <div>CK APP是一個以便捷化建中學生在學校的日常為宗旨而製作的建中校園APP，MAYBE MORE WORDS IN THE FUTURE BUT I DON'T KNOW WHAT TO TYPE HERE :＜</div>
+      <div>....CK APP 是一個革命性的建中校園應用程式，它的誕生源於一個崇高的使命：徹底改變建中學子的校園生活體驗。
+        這款app不僅僅是一個工具，它是每個建中人的得力助手、貼心夥伴和充滿智慧的嚮導。</div>
+      <div>....我們深知，建中學子們肩負著沉重的學業壓力和對未來的期許。正是為了減輕這份負擔，為了讓每一位建中人能夠更加從容、更加優雅地駕馭他們的學習生涯，CK APP應運而生。
+        它融合了最前沿的技術與最貼心的設計，將繁瑣的日常事務簡化為指尖輕觸的便利。</div>
     </q-banner>
 
     <div class="q-mt-md"></div>
@@ -66,8 +69,7 @@
     <q-banner class="bg-primary text-white text-h5">
       <div class="text-h5">聯絡我們</div>
       <template v-slot:action>
-        <q-btn flat color="white" label="Gmail" />
-        <q-btn flat color="white" label="Line" />
+        <q-btn flat color="white" label="Gmail" @click="openGmailCompose"/>
       </template>
     </q-banner>
 
@@ -84,9 +86,17 @@ import { ref } from 'vue'
 
 export default {
   setup() {
+    const openGmailCompose = () => {
+      const email = 'ckappofficial@gmail.com'
+      const subject = 'Questions and Recommendations regarding CK_APP'
+
+      const gmailComposeUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(email)}&su=${encodeURIComponent(subject)}`
+
+      window.open(gmailComposeUrl, '_blank')
+    }
     return{
-      slide: ref('style'),
-      lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
+      slide: ref('Intro'),
+      openGmailCompose
     }
   }
 };
--- CK_app/src/pages/HomePage.vue
+++ CK_app/src/pages/HomePage.vue
@@ -1,8 +1,70 @@
 <template>
-  <q-page>New Home Page</q-page>
+  <div class="app-container">
+    <div class="header">
+      <h5>歡迎使用 CK APP</h5>
+      <h6>為你量身打造的專屬校園APP</h6>
+    </div>
+    <div class="icon-grid">
+      <div v-for="item in items" :key="item.name" class="icon-item">
+        <img :src="item.icon" :alt="item.name">
+        <span>{{ item.name }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style></style>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      items: [
+        { name: 'Ride', icon: 'ride-icon.svg' },
+        { name: 'Food', icon: 'food-icon.svg' },
+        { name: 'Package', icon: 'package-icon.svg' },
+        { name: 'Reserve', icon: 'reserve-icon.svg' },
+        { name: 'Transit', icon: 'transit-icon.svg' },
+        { name: 'Rent', icon: 'rent-icon.svg' },
+        { name: 'Travel', icon: 'travel-icon.svg' },
+        { name: 'More', icon: 'more-icon.svg' },
+      ]
+    }
+  }
+}
 </script>
+
+<style scoped>
+.app-container {
+  padding: 20px;
+  font-weight: bold;
+}
+
+.header {
+  background-color:chocolate;
+  color: white;
+  padding-top: 0.1em;
+  padding-bottom: 0.1em;
+  padding-left: 1em;
+  border-radius: 15px;
+  margin-bottom: 20px;
+  font-weight: bolder;
+}
+
+.icon-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 15px;
+}
+
+.icon-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.icon-item img {
+  width: 50px;
+  height: 50px;
+  margin-bottom: 5px;
+}
+</style>

028be03686774b59aad6b42211c3ce15e6610fea
Kimi Yang <kimiyang951116@gmail.com>
2024-07-23 23:33:21 +0800

    update store folder

---
+++ CK_app/src/store/modules/schedule.js
@@ -0,0 +1,38 @@
+import scheduleData from "../../data/ClassesSchedule.json";
+
+export default {
+  state: () => ({
+    scheduleData: scheduleData,
+    userClass: "217",
+  }),
+  mutations: {
+    UPDATE_SCHEDULE(state, { rowIndex, colName, newValue }) {
+      state.scheduleData[rowIndex][colName] = newValue;
+    },
+    SET_SCHEDULE_DATA(state, data) {
+      state.scheduleData = data;
+    },
+    SET_USER_CLASS(state, newClass) {
+      state.userClass = newClass;
+    },
+  },
+  actions: {
+    updateSchedule({ commit }, payload) {
+      commit("UPDATE_SCHEDULE", payload);
+    },
+    setUserClass({ commit }, newClass) {
+      commit("SET_USER_CLASS", newClass);
+      localStorage.setItem("userClass", newClass);
+    },
+    loadSchedule({ commit, state }) {
+      const classSchedule = scheduleData[state.userClass]['schedule']
+      console.log(classSchedule)
+      commit("SET_SCHEDULE_DATA", classSchedule);
+    },
+  },
+  getters: {
+    getScheduleData: (state) => state.scheduleData,
+    getUserClass: (state) => state.userClass,
+  },
+};
+
